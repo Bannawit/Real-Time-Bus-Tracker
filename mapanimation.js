3d402ef9d47b9e348378ce85258c514480446ab8
Brent <Bannawit.KH@gmail.com>
2021-03-12 12:00:13 +0700

    Edit code 1

--- mapanimation.js
+++ mapanimation.js
@@ -1,27 +1,36 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYnJlbnRiYW5uYXdpdCIsImEiOiJja20wcWtoOG4xb2YzMndwOG14bTA2eXQxIn0.G4w4tHglL46PiwCggB2CsQ';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-71.104081, 42.365554],
+  zoom: 14
+});
 
-  var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [100.5383, 13.7649],
-      zoom: 14
-  });
- 
 var marker = new mapboxgl.Marker()
-    .setLngLat([100.5383, 13.7649])
-    .addTo(map);
+.setLngLat([-71.092761, 42.357575])
+.addTo(map);
 
 const busStops = [
-
+[-71.093729, 42.359244], 
+[-71.094915, 42.360175],
+[-71.095800, 42.360698],
+[-71.099558, 42.362953],
+[-71.103476, 42.365248],
+[-71.106067, 42.366806],
+[-71.108717, 42.368355],
+[-71.110799, 42.369192],
+[-71.113095, 42.370218],
+[-71.115476, 42.372085],
+[-71.117585, 42.373016],
+[-71.118625, 42.374863]
 ];
 
 let counter = 0;
 function move(){
-  setTimeout(()=>{
-    if (counter >= busStops.length) return;
-    marker.setLngLat(busStops[counter]);
-    counter++;
-    move();
-  },3000); 
-
+setTimeout(()=>{
+if (counter >= busStops.length) return;
+marker.setLngLat(busStops[counter]);
+counter++;
+move();
+},1000); 
 }
